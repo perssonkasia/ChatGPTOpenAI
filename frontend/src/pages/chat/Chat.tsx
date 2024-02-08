4fc42a521e35ea583ae088445216eceaa9ea1e76
perssonkasia <55349153+perssonkasia@users.noreply.github.com>
2024-02-08 09:49:31 +0100

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -624,7 +624,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Contoso}
+                                    src={TV2}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
